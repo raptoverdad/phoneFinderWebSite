847103f20da0af44bf9906a4978ec92285d6e746
Rodrigo Iván Miranda Reidenbach <rodrigomreidenbach@gmail.com>
2023-03-14 17:47:00 -0300

    app going to github repo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="logoDiv">
+      <img src="./assets/phoneFinderLogo.png" alt="">
+    </div>
+
+    <div class="options">
+      <router-link to="/">Menú principal</router-link> 
+      <router-link to="/about">Tutorial</router-link>
+    </div>
+
   </nav>
   <router-view/>
 </template>
@@ -11,14 +18,28 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
+*{
+  padding: auto;
+  margin: auto;
 }
-
+nav {
+  padding: 0;
+  background-color: #42b983;
+  min-width: 100vw;
+  height:min-content;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.options{
+background-color: aqua;
+}
+.logoDiv{
+  min-height: min-content;
+  background-color: black;
+}
 nav a {
   font-weight: bold;
   color: #2c3e50;
